26b937415fc9b87aac347a39ae969af8e1bba082
gifton <giftono@gmail.com>
2025-12-20 01:12:21 -0800

    Bump for critical bug fixes in VectorAccelerate

--- Package.swift
+++ Package.swift
@@ -46,7 +46,7 @@ let package = Package(
     dependencies: [
         // VSK dependencies - VectorAccelerate provides GPU-first indexing (v0.3.0+)
         .package(url: "https://github.com/gifton/VectorCore.git", from: "0.1.6"),
-        .package(url: "https://github.com/gifton/VectorAccelerate.git", from: "0.3.1"),
+        .package(url: "https://github.com/gifton/VectorAccelerate.git", from: "0.3.2"),
 
         // System dependencies
         .package(url: "https://github.com/apple/swift-log.git", from: "1.5.0"),
